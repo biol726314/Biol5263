56cfb50d4de327456e49e680f88a7c2698bbec76
biol726314 <alr8519@utulsa.edu>
2024-09-12 15:32:53 -0500

    added ecoli_map sbatch and sh files

---
+++ Scripts/chap2/BWA/ch2_ecoli_map_sort.sh
@@ -0,0 +1,2 @@
+samtools sort /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/mapping_to_reference/ecoli_mapped.bam \
+-o /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/mapping_to_reference/ecoli_mapped_sorted.bam
