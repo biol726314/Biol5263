4139d238cabfa8dc6214386a2067a5f719f9b232
biol726314 <alr8519@utulsa.edu>
2024-09-12 11:24:29 -0500

    Added Chap 2 try 2

---
+++ Scripts/ecoli_fastqc_try2.sh
@@ -0,0 +1,3 @@
+mkdir -p /scratch/biol726314/BIOL7263_Genomics/fastqc_output
+
+fastqc /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/read_1.fastq.gz -o /scratch/biol726314/BIOL7263_Genomics/fastqc_output/
