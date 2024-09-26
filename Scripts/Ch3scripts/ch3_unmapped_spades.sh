7e8944c0c596024924c4ba635cb785f24fc4ef3d
biol726314 <alr8519@utulsa.edu>
2024-09-26 15:12:16 -0500

    addded spades scripts

---
+++ Scripts/Ch3scripts/ch3_unmapped_spades.sh
@@ -0,0 +1,3 @@
+spades.py --careful -o /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/spades_assembly \
+-1 /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/unmapped_r1.fastq \
+-2 /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/unmapped_r2.fastq
