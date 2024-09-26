3aafbe7264c7b986da7c6c86af59664bdcd35aa6
biol726314 <alr8519@utulsa.edu>
2024-09-26 14:42:13 -0500

    added ch3 scripts

---
+++ Scripts/Ch3scripts/ch3_fastqc.sh
@@ -0,0 +1,4 @@
+mkdir -p /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/fastqc_output/
+
+fastqc /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/unmapped_r1.fastq -o /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/fastqc_output/
+fastqc /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/unmapped_r2.fastq -o /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/fastqc_output/
