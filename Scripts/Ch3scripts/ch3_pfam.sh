69ee391c289f10ab444ff8f32f89e3ed79afb353
biol726314 <alr8519@utulsa.edu>
2024-09-26 16:26:20 -0500

    add pfam scripts

---
+++ Scripts/Ch3scripts/ch3_pfam.sh
@@ -0,0 +1,7 @@
+hmmpress /scratch/biol726314/BIOL7263_Genomics/db/pfam/Pfam-A.hmm
+
+pfam_scan.pl -fasta /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/spades_assembly/contigs.orf.fasta \
+-dir /scratch/biol726314/BIOL7263_Genomics/db/pfam/ \
+-outfile /scratch/biol726314/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/spades_assembly/contigs.orf.pfam \
+-cpu 4 \
+-as
