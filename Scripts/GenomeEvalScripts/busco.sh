0856ad08c5e6398b19b01fdd70396df1d5936875
biol726314 <alr8519@utulsa.edu>
2024-10-10 16:09:44 -0500

    added busco scripts

---
+++ Scripts/GenomeEvalScripts/busco.sh
@@ -0,0 +1,3 @@
+ml BUSCO/5.2.2-foss-2020b
+
+busco -i /home/mbtoomey/BIOL7263_Genomics/precomp_hybrid_assembly/hybrid/contigs.fasta -m genome --lineage_dataset pseudomonadales_odb10 -c 20 --out pseud_busco2
